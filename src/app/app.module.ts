24b4d049cf1975a5e85a336de7e394ce01758b32
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2025-06-14 02:24:51 -0500

    move components into atomic design folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { InfoComponent } from './components/info/info.component';
-import { KnowledgeComponent } from './components/knowledge/knowledge.component';
-import { TitleSectionComponent } from './components/title-section/title-section.component';
-import { ProjectComponent } from './components/project/project.component';
-import { CardProjectComponent } from './components/card-project/card-project.component';
-import { ButtonComponent } from './components/button/button.component';
-import { TooltipComponent } from './components/tooltip/tooltip.component';
-import { TagComponent } from './components/tag/tag.component';
+import { NavbarComponent } from './components/molecules/navbar/navbar.component';
+import { InfoComponent } from './components/templates/info/info.component';
+import { KnowledgeComponent } from './components/templates/knowledge/knowledge.component';
+import { TitleSectionComponent } from './components/atoms/title-section/title-section.component';
+import { ProjectComponent } from './components/templates/project/project.component';
+import { CardProjectComponent } from './components/organisms/card-project/card-project.component';
+
+import { TooltipComponent } from './components/atoms/tooltip/tooltip.component';
+import { TagComponent } from './components/atoms/tag/tag.component';
 import {NgOptimizedImage} from "@angular/common";
-import { FooterComponent } from './components/footer/footer.component';
+import { FooterComponent } from './components/templates/footer/footer.component';
+import {ButtonComponent} from "./components/atoms/button/button.component";
 
 @NgModule({
   declarations: [
