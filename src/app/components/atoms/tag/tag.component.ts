025977c02893a0e29fd82791f1c26fb041dca949
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2026-09-05 23:07:08 -0500

    update tag styles

--- src/app/components/atoms/tag/tag.component.ts
+++ src/app/components/atoms/tag/tag.component.ts
@@ -1,18 +1,25 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {TECHNOLOGY_COLORS, TechnologyColor} from "../../../shared/contants/tag.constants";
 
 @Component({
-  selector: 'app-tag',
-  templateUrl: './tag.component.html',
-  styleUrls: ['./tag.component.scss']
+    selector: 'app-tag',
+    templateUrl: './tag.component.html',
+    styleUrls: ['./tag.component.scss']
 })
 export class TagComponent implements OnInit {
-  @Input() src: string = "";
-  @Input() tagName: string = "N/A";
+    @Input() src: string = "";
+    @Input() tagName: string = "";
+    protected readonly technologyColors = TECHNOLOGY_COLORS;
 
-  constructor() { }
+    constructor() {
+    }
 
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
+
+    get colors(): TechnologyColor {
+        return this.technologyColors[this.tagName] ?? {background: '#EEEEEE', text: '#333333',};
+    }
 
 }
